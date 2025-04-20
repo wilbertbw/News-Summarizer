caeee4ba2a4a6fb155ac9ddb4c6acb03e1d6d099
wilbertbw <wilbertbwibowo@gmail.com>
2025-04-20 00:50:14 -0400

    first commit

---
+++ news-summarizer.py
@@ -0,0 +1,12 @@
+'''
+Write a Python program that visits cbc.ca/news
+
+The program should “read” the first 7 news articles and then use an LLM to create a short summary of each article.
+
+The program will output the article headline and the summary produced by AI
+
+Tip:  use any LLM you want for this task.  For example:  https://console.groq.com/home
+
+Note: You will not be judged on the quality of the LLM’s output.
+'''
+
